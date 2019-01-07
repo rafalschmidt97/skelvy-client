fc4d27b176f7fa7741b36d2f7282176fdbfdb573
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-07 19:53:47 +0100

    feature(core) add storage provider
    - first native plugin require resources directory (created automaticly)
    - update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage';
+
+const libraries = [IonicStorageModule.forRoot()];
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ...libraries,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
