c3bd5f57eb3752b5ad84712536bb981575d42494
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-07 18:09:29 +0100

    feature(core) add sample pages to present bootstrap with ionic
    - new about page
    - update home page
    - update routing

---
+++ src/app/about/about.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+})
+export class AboutPage {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {}
+export class HomePage {
+  constructor(public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: 'This is an alert message.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+}
