70a412384dd2184ed179151f4375a8cfc17cb532
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-08 14:00:30 +0100

    refactor(app) new project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
+  {
+    path: '',
+    loadChildren: './modules/welcome.module#WelcomeModule',
+  },
 ];
 
 @NgModule({
