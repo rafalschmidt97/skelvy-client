a31bbb73af5cd9f992d5e7e6acbaf59d0b6e6362
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-08 09:41:05 +0100

    feature(core) set language in storage
    - simplify setLanguage method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageConstants } from './common/i18n/language.constants';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,7 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private translateService: TranslateService,
+    private storage: Storage,
   ) {
     this.initializeApp();
     this.setLanguage();
@@ -32,26 +34,20 @@ export class AppComponent {
     this.translateService.addLangs(LanguageConstants.SUPPORTED_LANGUAGES);
     this.translateService.setDefaultLang(LanguageConstants.DEFAULT_LANGUAGE);
 
-    // TODO: ADD STORAGE HERE
-    // const language = this.storage.getItem('language');
+    this.storage.get('language').then(language => {
+      if (!language) {
+        const browserLanguage = this.translateService.getBrowserLang();
+        language = browserLanguage.match(LanguageConstants.LANGUAGES_REGEX)
+          ? browserLanguage
+          : LanguageConstants.DEFAULT_LANGUAGE;
 
-    let language = LanguageConstants.DEFAULT_LANGUAGE;
+        this.storage.set('language', language);
+      }
 
-    if (language) {
-      language = language.match(LanguageConstants.LANGUAGES_REGEX)
-        ? language
-        : LanguageConstants.DEFAULT_LANGUAGE;
-    } else {
-      const browserLanguage = this.translateService.getBrowserLang();
-      language = browserLanguage.match(LanguageConstants.LANGUAGES_REGEX)
-        ? browserLanguage
-        : LanguageConstants.DEFAULT_LANGUAGE;
-    }
+      this.translateService.use(language);
 
-    this.translateService.use(language);
-    // TODO: ADD STORAGE HERE
-    // this.storage.setItem('language', newLanguage);
-    // TODO: ADD MOMENT HERE
-    // moment.locale(newLanguage);
+      // TODO: ADD MOMENT HERE
+      // moment.locale(newLanguage);
+    });
   }
 }
