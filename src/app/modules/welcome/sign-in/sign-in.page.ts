becf3950ac5b6868209f6c278b524d17e74b93c4
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-09 09:48:28 +0100

    feature(welcome) add sign in page

--- src/app/modules/welcome/sign-in/sign-in.page.ts
+++ src/app/modules/welcome/sign-in/sign-in.page.ts
@@ -1,8 +1,4 @@
 import { Component, TemplateRef } from '@angular/core';
-import { Alert } from '../../../shared/alert/alert';
-import { AlertService } from '../../../shared/alert/alert.service';
-import { Modal } from '../../../shared/modal/modal';
-import { ModalService } from '../../../shared/modal/modal.service';
 import { IframeService } from '../../../shared/iframe/iframe.service';
 import { Iframe } from '../../../shared/iframe/iframe';
 
@@ -12,38 +8,12 @@ import { Iframe } from '../../../shared/iframe/iframe';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage {
-  alert: Alert;
-  modal: Modal;
   iframe: Iframe;
 
-  constructor(
-    private readonly alertService: AlertService,
-    private readonly modalService: ModalService,
-    private readonly iframeService: IframeService,
-  ) {}
-
-  showAlert(template: TemplateRef<any>) {
-    this.alert = this.alertService.show(template);
-  }
-
-  showModal(template: TemplateRef<any>) {
-    this.modal = this.modalService.show(template);
-  }
-
+  constructor(private readonly iframeService: IframeService) {}
+  
   showIframe(template: TemplateRef<any>) {
     this.iframe = this.iframeService.show(template);
-  }
-
-  confirmAlert() {
-    this.alert.hide();
-  }
-
-  declineAlert() {
-    this.alert.hide();
-  }
-
-  declineModal() {
-    this.modal.hide();
   }
 
   declineIframe() {
