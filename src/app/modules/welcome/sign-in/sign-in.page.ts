bfe647ba886e595cb9fd74522061e136fcd6def8
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-09 16:18:00 +0100

    feature(welcome) add sample use of modal

--- src/app/modules/welcome/sign-in/sign-in.page.ts
+++ src/app/modules/welcome/sign-in/sign-in.page.ts
@@ -1,6 +1,8 @@
 import { Component, TemplateRef } from '@angular/core';
 import { Alert } from '../../../shared/alert/alert';
 import { AlertService } from '../../../shared/alert/alert.service';
+import { Modal } from '../../../shared/modal/modal';
+import { ModalService } from '../../../shared/modal/modal.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,18 +11,30 @@ import { AlertService } from '../../../shared/alert/alert.service';
 })
 export class SignInPage {
   alert: Alert;
+  modal: Modal;
 
-  constructor(private readonly alertService: AlertService) {}
+  constructor(
+    private readonly alertService: AlertService,
+    private readonly modalService: ModalService,
+  ) {}
 
   showAlert(template: TemplateRef<any>) {
     this.alert = this.alertService.show(template);
   }
 
-  confirm() {
-    this.decline();
+  showModal(template: TemplateRef<any>) {
+    this.modal = this.modalService.show(template);
   }
 
-  decline() {
+  confirmAlert() {
     this.alert.hide();
+  }
+
+  declineAlert() {
+    this.alert.hide();
+  }
+
+  declineModal() {
+    this.modal.hide();
   }
 }
