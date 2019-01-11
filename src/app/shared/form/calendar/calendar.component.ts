a6b0ebec6a13cc9f052a9fed0cd500372788bdfd
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-11 14:20:27 +0100

    feature(shared) add calendar component to form module

---
+++ src/app/shared/form/calendar/calendar.component.ts
@@ -0,0 +1,84 @@
+import {
+  Component,
+  forwardRef,
+  Inject,
+  Input,
+  OnInit,
+  TemplateRef,
+} from '@angular/core';
+import { ComplexFieldComponent } from '../complex-field.component';
+import { LocaleSettings } from 'primeng/primeng';
+import * as moment from 'moment';
+import { FormComponent } from '../form.component';
+import { Modal } from '../../modal/modal';
+import { ModalService } from '../../modal/modal.service';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss'],
+})
+export class CalendarComponent extends ComplexFieldComponent implements OnInit {
+  @Input() range: boolean;
+  @Input() min: number;
+  @Input() max: number;
+
+  locale: LocaleSettings = {
+    firstDayOfWeek: moment.localeData().firstDayOfWeek(),
+    dayNames: moment.weekdays(),
+    dayNamesShort: moment.weekdaysShort(),
+    dayNamesMin: moment.weekdaysMin(),
+    monthNames: moment.months('MMMM'),
+    monthNamesShort: moment.weekdaysShort(),
+    today: '',
+    clear: '',
+  };
+
+  today = new Date();
+  modal: Modal;
+
+  constructor(
+    @Inject(forwardRef(() => FormComponent)) readonly parent: FormComponent,
+    private readonly modalService: ModalService,
+  ) {
+    super(parent);
+  }
+
+  get dateLabel(): string {
+    const start = this.form.get(this.name).value[0];
+    const end = this.form.get(this.name).value[1] || null;
+
+    if (end) {
+      return `${moment(start).format('DD.MM.YYYY')} - ${moment(end).format(
+        'DD.MM.YYYY',
+      )}`;
+    }
+
+    return moment(start).format('DD.MM.YYYY');
+  }
+
+  ngOnInit() {
+    this.form.get(this.name).valueChanges.subscribe((value: Date[]) => {
+      const hasSameDates =
+        value[1] && value[0].getTime() === value[1].getTime();
+      if (hasSameDates) {
+        this.form.patchValue({
+          [this.name]: [value[0], null],
+        });
+      }
+    });
+  }
+
+  open(template: TemplateRef<any>) {
+    if (!this.isLoading) {
+      this.modal = this.modalService.show(template, {
+        ignoreBackdropClick: true,
+        keyboard: false,
+      });
+    }
+  }
+
+  confirm() {
+    this.modal.hide();
+  }
+}
--- src/app/shared/form/form.module.ts
+++ src/app/shared/form/form.module.ts
@@ -6,6 +6,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { TextareaComponent } from './textarea/textarea.component';
 import { DynamicHeightDirective } from './textarea/dynamic-height.directive';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarModule } from 'primeng/primeng';
+import { ModalModule } from '../modal/modal.module';
 
 @NgModule({
   declarations: [
@@ -13,13 +16,21 @@ import { DynamicHeightDirective } from './textarea/dynamic-height.directive';
     InputComponent,
     TextareaComponent,
     DynamicHeightDirective,
+    CalendarComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule, TranslateModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    ModalModule,
+    CalendarModule,
+  ],
   exports: [
     ReactiveFormsModule,
     FormComponent,
     InputComponent,
     TextareaComponent,
+    CalendarComponent,
   ],
 })
 export class FormModule {}
