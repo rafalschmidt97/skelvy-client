d04672561a181eea55caaa38f24073c7ab261b00
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-11 16:40:07 +0100

    refactor(shared) change cross to done text on decline button for modal and iframe

--- src/app/shared/iframe/iframe.module.ts
+++ src/app/shared/iframe/iframe.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeUrlPipe } from './safe-url.pipe';
 import { IframeComponent } from './iframe.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SafeUrlPipe, IframeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   exports: [IframeComponent],
 })
 export class IframeModule {}
--- src/app/shared/modal/modal.module.ts
+++ src/app/shared/modal/modal.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   exports: [ModalComponent],
 })
 export class ModalModule {}
