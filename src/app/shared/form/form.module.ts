ff51dff257fa2a68e45bf23057fd101d9e8c9b65
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-11 17:11:33 +0100

    feature(shared) add date component to form module

--- src/app/shared/form/form.module.ts
+++ src/app/shared/form/form.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form.component';
 import { InputComponent } from './input/input.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { TextareaComponent } from './textarea/textarea.component';
 import { DynamicHeightDirective } from './textarea/dynamic-height.directive';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarModule } from 'primeng/primeng';
 import { ModalModule } from '../modal/modal.module';
+import { DateComponent } from './date/date.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -17,13 +19,16 @@ import { ModalModule } from '../modal/modal.module';
     TextareaComponent,
     DynamicHeightDirective,
     CalendarComponent,
+    DateComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     TranslateModule,
     ModalModule,
     CalendarModule,
+    IonicModule,
   ],
   exports: [
     ReactiveFormsModule,
@@ -31,6 +36,7 @@ import { ModalModule } from '../modal/modal.module';
     InputComponent,
     TextareaComponent,
     CalendarComponent,
+    DateComponent,
   ],
 })
 export class FormModule {}
