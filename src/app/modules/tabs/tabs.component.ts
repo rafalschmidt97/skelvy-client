cfe37b58bf5d2590e262f9a60de2b758fdba329b
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-01-20 23:19:38 +0100

    feature(tabs) hide tab bar on chat

--- src/app/modules/tabs/tabs.component.ts
+++ src/app/modules/tabs/tabs.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Meeting } from '../meeting/meeting';
+import { MeetingStoreService } from '../meeting/meeting-store.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
 })
-export class TabsComponent {}
+export class TabsComponent {
+  meeting$: Observable<Meeting>;
+
+  constructor(
+    private readonly store: MeetingStoreService,
+    private readonly router: Router,
+  ) {
+    this.meeting$ = store.data;
+  }
+
+  get hiddenNavigation() {
+    return this.router.url === '/tabs/chat';
+  }
+}
