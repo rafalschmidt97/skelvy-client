da54adb309bd483b896ae6997b067f3356453b54
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-02-27 20:13:05 +0100

    bugfix(meeting) do not throw error for not existing meeting in resolver

--- src/app/modules/meeting/meeting.resolver.ts
+++ src/app/modules/meeting/meeting.resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Resolve } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { ToastService } from '../../core/toast/toast.service';
@@ -7,6 +7,8 @@ import { _ } from '../../core/i18n/translate';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MeetingService } from './meeting.service';
 import { MeetingModel } from './meeting';
+import { AuthService } from '../../core/auth/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root',
@@ -15,16 +17,23 @@ export class MeetingResolver implements Resolve<MeetingModel> {
   constructor(
     private readonly meetingService: MeetingService,
     private readonly toastService: ToastService,
+    private readonly authService: AuthService,
+    private readonly routerNavigation: NavController,
   ) {}
 
   resolve(): Observable<MeetingModel> {
     return this.meetingService.getMeeting().pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status !== 404) {
-          this.toastService.createError(_('Something went wrong'));
+          this.authService.logout().then(() => {
+            this.routerNavigation.navigateBack(['/home']);
+            this.toastService.createError(_('Something went wrong'));
+          });
+
+          return throwError(error);
         }
 
-        return throwError(error);
+        return of(null);
       }),
     );
   }
