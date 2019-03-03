daea7bc05c28348aa867a4db3f0f0a6761059ea3
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-03-03 16:32:17 +0100

    bugfix(shared) make subject protected and fix get data

--- src/app/shared/store.service.ts
+++ src/app/shared/store.service.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 
 export abstract class StoreService<T> {
-  private readonly subject: BehaviorSubject<T> = new BehaviorSubject(null);
+  protected readonly subject: BehaviorSubject<T> = new BehaviorSubject(null);
   readonly data$: Observable<T> = this.subject.asObservable();
 
   set(data: T) {
@@ -9,6 +9,6 @@ export abstract class StoreService<T> {
   }
 
   get data() {
-    return this.subject.value;
+    return this.subject.getValue();
   }
 }
