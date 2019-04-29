abdce85a44aaa7eb3ee0a61ddcf5e2b03ed0604d
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-04-29 08:49:38 +0200

    bugfix(users) listen for remove/disable action on socket

--- src/app/modules/user/user-socket.service.ts
+++ src/app/modules/user/user-socket.service.ts
@@ -10,6 +10,8 @@ import {
 import { SessionService } from '../../core/auth/session.service';
 import { ToastService } from '../../core/toast/toast.service';
 import { MeetingSocketService } from '../meeting/meeting-socket.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +25,8 @@ export class UserSocketService {
     private readonly sessionService: SessionService,
     private readonly toastService: ToastService,
     private readonly meetingSocket: MeetingSocketService,
+    private readonly authService: AuthService,
+    private readonly routerNavigation: NavController,
   ) {
     this.socket = new HubConnectionBuilder()
       .withUrl(environment.apiUrl + 'users', {
@@ -40,6 +44,8 @@ export class UserSocketService {
   connect() {
     if (!this.initialized) {
       this.meetingSocket.onMeetingActions();
+      this.onUserRemoved();
+      this.onUserDisabled();
       this.onClose();
       this.connectToSocket();
 
@@ -79,6 +85,32 @@ export class UserSocketService {
         this.toastService.createWarning(_('The connection has been lost'));
       }
       this.disconnecting = false;
+    });
+  }
+
+  private onUserRemoved() {
+    this.socket.on('UserRemoved', () => {
+      this.authService.logoutWithoutRequest().then(() => {
+        this.routerNavigation.navigateBack(['/home/sign-in']);
+        this.toastService.createError(
+          _(
+            `Your account has been removed. If you have any questions, don't hesitate to contact us by using our website`,
+          ),
+        );
+      });
+    });
+  }
+
+  private onUserDisabled() {
+    this.socket.on('UserDisabled', () => {
+      this.authService.logoutWithoutRequest().then(() => {
+        this.routerNavigation.navigateBack(['/home/sign-in']);
+        this.toastService.createError(
+          _(
+            `Your account has been disabled. If you have any questions, don't hesitate to contact us by using our website`,
+          ),
+        );
+      });
     });
   }
 
