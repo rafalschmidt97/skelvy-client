73677d3206fe06dd529a9ddfe83c4e0149d0b689
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-05-22 13:08:04 +0200

    feature(meetings) save last meeting preferences

--- src/app/modules/meeting/edit/edit.page.ts
+++ src/app/modules/meeting/edit/edit.page.ts
@@ -11,6 +11,8 @@ import { NavController } from '@ionic/angular';
 import { ToastService } from '../../../core/toast/toast.service';
 import { MeetingSocketService } from '../meeting-socket.service';
 import { UserStoreService } from '../../user/user-store.service';
+import { Storage } from '@ionic/storage';
+import { isNil } from 'lodash';
 
 @Component({
   selector: 'app-edit',
@@ -37,7 +39,7 @@ export class EditPage implements Form, OnSubmit, OnInit {
     _('vodka'),
     _('whiskey'),
   ];
-  loadingDrinks = true;
+  loadingForm = true;
 
   constructor(
     private readonly formBuilder: FormBuilder,
@@ -46,6 +48,7 @@ export class EditPage implements Form, OnSubmit, OnInit {
     private readonly routerNavigation: NavController,
     private readonly toastService: ToastService,
     private readonly userStore: UserStoreService,
+    private readonly storage: Storage,
   ) {
     this.form = this.formBuilder.group({
       date: [[this.today, this.tomorrow], Validators.required],
@@ -65,44 +68,19 @@ export class EditPage implements Form, OnSubmit, OnInit {
           };
         });
 
-        this.form.patchValue({
-          drinks: [
-            this.drinks[1].value,
-            this.drinks[2].value,
-            this.drinks[3].value,
-          ],
-        });
-
-        this.loadingDrinks = false;
+        this.fillForm();
       },
       () => {
-        this.loadingDrinks = false;
+        this.loadingForm = false;
         this.toastService.createError(
           _('A problem occurred while finding drinks'),
         );
       },
     );
-
-    const userAge = moment().diff(
-      this.userStore.data.profile.birthday,
-      'years',
-    );
-
-    if (userAge >= 21 && userAge < 52) {
-      this.form.patchValue({
-        age: [userAge - 3, userAge + 3],
-      });
-    } else {
-      if (userAge > 52) {
-        this.form.patchValue({
-          age: [45, 55],
-        });
-      }
-    }
   }
 
   onSubmit() {
-    if (this.form.valid && !this.isLoading && !this.loadingDrinks) {
+    if (this.form.valid && !this.isLoading && !this.loadingForm) {
       this.isLoading = true;
       const form = this.form.value;
       const request: MeetingRequestDto = {
@@ -119,6 +97,8 @@ export class EditPage implements Form, OnSubmit, OnInit {
 
       this.meetingService.createMeetingRequest(request).subscribe(
         () => {
+          this.storage.set('lastMeetingRequestForm', form);
+
           this.meetingService.findMeeting().subscribe(
             () => {
               if (window.history.length > 1) {
@@ -142,5 +122,66 @@ export class EditPage implements Form, OnSubmit, OnInit {
         },
       );
     }
+  }
+
+  private fillForm() {
+    this.storage
+      .get('lastMeetingRequestForm')
+      .then(requestForm => {
+        if (!isNil(requestForm)) {
+          const minDate = new Date(requestForm.date[0]);
+          const minDateValidated = minDate >= this.today ? minDate : this.today;
+          const maxDate = requestForm.date[1]
+            ? new Date(requestForm.date[1])
+            : null;
+          const maxDateValidated = maxDate
+            ? maxDate > minDateValidated
+              ? maxDate
+              : moment(minDateValidated)
+                  .add(1, 'days')
+                  .startOf('day')
+                  .toDate()
+            : null;
+
+          this.form.patchValue({
+            date: maxDateValidated
+              ? [minDateValidated, maxDateValidated]
+              : [minDateValidated],
+            address: requestForm.address,
+            age: requestForm.age,
+            drinks: requestForm.drinks,
+          });
+        } else {
+          this.form.patchValue({
+            drinks: [
+              this.drinks[1].value,
+              this.drinks[2].value,
+              this.drinks[3].value,
+            ],
+          });
+
+          const userAge = moment().diff(
+            this.userStore.data.profile.birthday,
+            'years',
+          );
+
+          if (userAge >= 21 && userAge < 52) {
+            this.form.patchValue({
+              age: [userAge - 3, userAge + 3],
+            });
+          } else {
+            if (userAge > 52) {
+              this.form.patchValue({
+                age: [45, 55],
+              });
+            }
+          }
+        }
+
+        this.loadingForm = false;
+      })
+      .catch(() => {
+        this.loadingForm = false;
+      });
   }
 }
