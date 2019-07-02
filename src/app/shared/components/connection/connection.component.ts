ee62cace054090eecc13249a873548cc0db5e3c7
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-07-02 17:44:07 +0200

    feature(shared) add animation to connection bar

--- src/app/shared/components/connection/connection.component.ts
+++ src/app/shared/components/connection/connection.component.ts
@@ -3,11 +3,38 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Connection } from '../../../core/state';
 import { StateStoreService } from '../../../core/state-store.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-connection',
   templateUrl: './connection.component.html',
   styleUrls: ['./connection.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'true',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)',
+        }),
+      ),
+      state(
+        'false',
+        style({
+          opacity: 0,
+          transform: 'translateY(-20px)',
+        }),
+      ),
+      transition('0 => 1', animate('0.1s')),
+      transition('1 => 0', animate('0.1s 1s')),
+    ]),
+  ],
 })
 export class ConnectionComponent {
   connection$: Observable<Connection>;
