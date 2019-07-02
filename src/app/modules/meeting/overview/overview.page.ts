c43589764bdf4a96c6a8357b793c80c118dc9ba3
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-07-02 18:51:20 +0200

    feature(app) add spinner while syncing meeting data

--- src/app/modules/meeting/overview/overview.page.ts
+++ src/app/modules/meeting/overview/overview.page.ts
@@ -3,23 +3,27 @@ import { MeetingModel } from '../meeting';
 import { MeetingStoreService } from '../meeting-store.service';
 import { Observable } from 'rxjs';
 import { UserStoreService } from '../../user/user-store.service';
-import { UserDto } from '../../user/user';
-import { MeetingService } from '../meeting.service';
+import { UserModel } from '../../user/user';
+import { StateStoreService } from '../../../core/state/state-store.service';
+import { StateModel } from '../../../core/state/state';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.page.html',
+  styleUrls: ['./overview.page.scss'],
 })
 export class OverviewPage {
   meeting$: Observable<MeetingModel>;
-  user$: Observable<UserDto>;
+  user$: Observable<UserModel>;
+  state$: Observable<StateModel>;
 
   constructor(
     private readonly meetingStore: MeetingStoreService,
-    private readonly meetingService: MeetingService,
     private readonly userStore: UserStoreService,
+    private readonly stateStore: StateStoreService,
   ) {
     this.meeting$ = meetingStore.data$;
     this.user$ = userStore.data$;
+    this.state$ = stateStore.data$;
   }
 }
