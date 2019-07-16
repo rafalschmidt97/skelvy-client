76c4b0915d1e410932230c3e1d103388d0e91936
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-07-16 19:10:25 +0200

    feature(app) add background notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { Camera } from '@ionic-native/camera/ngx';
 import { Crop } from '@ionic-native/crop/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { File } from '@ionic-native/file/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 if (!environment.production) {
   registerReduxDevToolOnDevice();
@@ -73,6 +74,7 @@ if (!environment.production) {
     Crop,
     WebView,
     File,
+    LocalNotifications,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {
       provide: NGXS_PLUGINS,
