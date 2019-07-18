a743915a3f78e10ac715546a2d65a2af57ce2715
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-07-18 11:38:59 +0200

    bugfix(chat) hide statusbar on image preview

--- src/app/modules/chat/overview/image-viewer/image-viewer.component.ts
+++ src/app/modules/chat/overview/image-viewer/image-viewer.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.scss'],
 })
-export class ImageViewerComponent {
+export class ImageViewerComponent implements OnInit {
   @Input() src = '';
   slideOptions = {
     centeredSlides: 'true',
   };
+  constructor(
+    private readonly modalController: ModalController,
+    private readonly statusBar: StatusBar,
+  ) {}
 
-  constructor(private readonly modalController: ModalController) {}
+  ngOnInit() {
+    this.statusBar.hide();
+  }
 
   closeModal() {
+    this.statusBar.show();
     this.modalController.dismiss();
   }
 }
