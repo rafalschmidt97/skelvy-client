bf05238d57f3362620a3ea7d0acb59d1265516c6
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-12-28 03:36:36 +0100

    build: release version 2.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import * as npm from '../../package.json';
 export const environment = {
   production: true,
   version: npm.version,
+  hostname: npm.config.hostname,
   apiUrl: 'https://api.skelvy.com/',
-  versionApiUrl: 'https://api.skelvy.com/v1.0/',
+  versionApiUrl: 'https://api.skelvy.com/v2.0/',
 };
